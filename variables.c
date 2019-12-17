6d85db4d121614c1cc63b99f88356fd56a23a447
Brian Slonim <slonimb2324@my.sduhsd.net>
2019-12-17 12:13:52 -0800

    adding variables code

---
+++ variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  int a;
+  int b = 3;
+  int c;
+
+  a = 2;
+  c = a + b;
+  printf("Sum of %d and %d is %d\n", a, b, c);
+  printf("a is now %d and c is now %d\nSwitching now...\n", a, c);
+
+  b = a;
+  a = c;
+  c= b;
+
+  printf("a is now %d and c is now %d\n", a, c);
+}
